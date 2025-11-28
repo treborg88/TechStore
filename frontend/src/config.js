33c799a87719db8f90ca20fa664b5ce9d870cfd8
treborg88 <143137923+treborg88@users.noreply.github.com>
2025-11-28 00:34:56 -0400

    config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,11 +1,11 @@
 // config.js - Configuración centralizada de la aplicación
 
-export const API_URL = 'http://143.47.118.165:5001/api';
-export const BASE_URL = 'http://143.47.118.165:5001';
+// export const API_URL = 'http://143.47.118.165:5001/api';
+// export const BASE_URL = 'http://143.47.118.165:5001';
 
 
-// export const API_URL = 'http://localhost:5001/api';
-// export const BASE_URL = 'http://localhost:5001';
+export const API_URL = 'http://localhost:5001/api';
+export const BASE_URL = 'http://localhost:5001';
 
 
 
